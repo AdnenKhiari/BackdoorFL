8a74cb30fec41b94e2f9c45d002bd4eb29113584
AdnenKhiari <adnenkhiari484@gmail.com>
2024-08-19 09:28:02 +0100

    breast fix

--- dataset/breast.py
+++ dataset/breast.py
@@ -10,30 +10,4 @@ class Breast(Dataset):
         super(Breast,self).__init__(partitioner)
         
     def get_federated_dataset(self,partitioners):
-        fd= FederatedDataset(dataset="EulerianKnight/breast-histopathology-images-train-test-valid-split",partitioners=partitioners)
-        print("SP",fd._check_if_split_present("train"))
-        print("SP",fd._check_if_split_present("test"))
-    def apply_transforms(self):
-        def tr(batch):
-            print("BB",batch)
-            pytorch_transforms = Compose(
-                [ToTensor()]
-            )
-            batch["image"] = [pytorch_transforms(image) for image in batch["img"]]
-            del batch["img"]
-            return batch
-        return tr
-    def collate(self):
-        def col(batch):
-            print("BB",batch)
-
-            images = []
-            labels = []
-            for item in batch:
-                images.append(item["image"])
-                labels.append(torch.tensor(item["label"]))
-            return {
-                "image": torch.stack(images),
-                "label": torch.stack(labels)
-            }
-        return col
+        return FederatedDataset(dataset="dbzadnen/breast-histopathology-images",partitioners=partitioners)
