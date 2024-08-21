32b12abfec7419b047504045cf1ff1be99905aaf
AdnenKhiari <adnenkhiari484@gmail.com>
2024-08-21 15:49:55 +0100

    fix: fixed metrics

--- server.py
+++ server.py
@@ -87,11 +87,11 @@ def aggregation_metrics(client_metrics: List[Tuple[int, Dict[str, float]]]) -> D
         if total_weights[key] > 0:
             aggregated_metrics[key] = weighted_sums[key] / total_weights[key]
     
-    result = {
-        **poisoning_stats,
-        "metrics": aggregated_metrics
-    }
-    return result
+    # result = {
+    #     **poisoning_stats,
+    #     "metrics": aggregated_metrics
+    # }
+    return aggregated_metrics
 
 def get_evalulate_fn(model_cfg: int, testloader,data_poisoner: DataPoisoner):
     """Return a function to evaluate the global model."""
