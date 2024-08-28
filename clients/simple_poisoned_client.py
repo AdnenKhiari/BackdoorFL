717cc4219c5349362bbc1ece34ce72c19a8ffcec
AdnenKhiari <adnenkhiari484@gmail.com>
2024-08-28 15:55:16 +0100

    wdb

--- clients/simple_poisoned_client.py
+++ clients/simple_poisoned_client.py
@@ -11,7 +11,7 @@ class SimplePoisonedClient(PoisonedFlowerClient):
         data_poisoner = DataPoisoningPipeline([SimplePatchPoisoner((20,20),(5,5),1)])
         super(SimplePoisonedClient,self).__init__(node_id,model_cfg,optimizer,data_poisoner,batch_poison_num,target_poisoned,batch_size)
     def set_parameters(self, parameters):
-        self.model_poisoner = ModelPoisoningPipeline([ModelReplacement(parameters)])
+        self.model_poisoner = ModelPoisoningPipeline([ModelReplacement(parameters,10)])
         # self.model_poisoner = ModelPoisoningPipeline([])
         return super().set_parameters(parameters)
     def report_data(self):
