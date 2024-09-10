01f35b70ff324cd05339309dcc8f7b804aa25881
AdnenKhiari <adnenkhiari484@gmail.com>
2024-09-10 11:48:12 +0100

    fixd

--- custom_simulation/client_manager.py
+++ custom_simulation/client_manager.py
@@ -6,6 +6,7 @@ import random
 
 from flwr.server.client_proxy import ClientProxy
 from flwr.server.criterion import Criterion
+import numpy as np
 
 class ClientM(SimpleClientManager):
     
@@ -13,9 +14,36 @@ class ClientM(SimpleClientManager):
         # random.seed(seed)
         super().__init__()
         
-    def sample(self, num_clients: int, min_num_clients: int | None = None, criterion: Criterion | None = None) -> List[ClientProxy]:
-        print("State",random.getstate())
-        result = super().sample(num_clients, min_num_clients, criterion)
+    def sample(
+        self,
+        num_clients: int,
+        min_num_clients: Optional[int] = None,
+        criterion: Optional[Criterion] = None,
+    ) -> List[ClientProxy]:
+        """Sample a number of Flower ClientProxy instances."""
+        # Block until at least num_clients are connected.
+        if min_num_clients is None:
+            min_num_clients = num_clients
+        self.wait_for(min_num_clients)
+        # Sample clients which meet the criterion
+        available_cids = list(self.clients)
+        if criterion is not None:
+            available_cids = [
+                cid for cid in available_cids if criterion.select(self.clients[cid])
+            ]
+
+        if num_clients > len(available_cids):
+            log(
+                INFO,
+                "Sampling failed: number of available clients"
+                " (%s) is less than number of requested clients (%s).",
+                len(available_cids),
+                num_clients,
+            )
+            return []
+
+        sampled_cids = np.random.sample(available_cids, num_clients)
+        result = [self.clients[cid] for cid in sampled_cids]
         print("Sampled",list(map(lambda d: d.node_id,result)))
         return result
         
