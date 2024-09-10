ca40f36d4707478dab8c2e83a7a18655998967b3
AdnenKhiari <adnenkhiari484@gmail.com>
2024-09-10 11:06:39 +0100

    fixd

--- custom_simulation/client_manager.py
+++ custom_simulation/client_manager.py
@@ -4,10 +4,16 @@ from flwr.common.logger import log
 from logging import INFO
 import random
 
+from flwr.server.client_proxy import ClientProxy
+from flwr.server.criterion import Criterion
+
 class ClientM(SimpleClientManager):
     
     def __init__(self,seed):
         # random.seed(seed)
         super().__init__()
         
+    def sample(self, num_clients: int, min_num_clients: int | None = None, criterion: Criterion | None = None) -> List[ClientProxy]:
+        result = super().sample(num_clients, min_num_clients, criterion)
+        print("Sampled",list(map(lambda d: d.node_id,result)))
         
