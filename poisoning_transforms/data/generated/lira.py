e91681ee6c54497969b6d320408b76c0afb2731a
AdnenKhiari <adnenkhiari484@gmail.com>
2024-09-11 13:13:58 +0100

    vit

--- poisoning_transforms/data/generated/lira.py
+++ poisoning_transforms/data/generated/lira.py
@@ -20,8 +20,7 @@ class LiraGenerator(DataPoisoner):
         
     def get_poisoned_batch(self,model,images):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        images = copy.deepcopy(images).to(device)
-        noise = model(images) * self.eps
+        noise = model(images.to(device)) * self.eps
         images = LiraGenerator.clamp(images + noise,0,1)
         return images
     
@@ -44,40 +43,70 @@ class LiraGenerator(DataPoisoner):
                 "image": self.get_poisoned_batch(self.attack_model,img),
                 "label": data["label"]
             }
-    
+            
     def train(self):
         
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.client_model.eval()
         self.client_model.to(device)
-        
-        # prepare the hypothical model for the attacker to learn better
-        tmp_model = copy.deepcopy(self.client_model)
-        tmp_model.train()
-        tmp_model.to(device)
-        tempmodel_optimizer = torch.optim.SGD(tmp_model.parameters(),lr=self.client_model_optimizer_lr)
-        
-        lira_optimizer = torch.optim.SGD(self.attack_model.parameters(),lr=self.attacker_train_optimizer_lr)
+        lira_optimizer = torch.optim.Adam(self.attack_model.parameters(),lr=self.attacker_train_optimizer_lr)
         
         self.attack_model.to(device)
         self.attack_model.train()
-        k_lira = 2
         for epoch in range(self.attacker_train_epoch):
             total_attack_loss = 0
             total_tempmodel_loss = 0
 
-            for i in range(k_lira):
-                for data in self.train_loader:
-                    attack_loss = self.train_lira_batch(data,device,tmp_model,lira_optimizer)
-                    with torch.no_grad():
-                        total_attack_loss += attack_loss
-            total_attack_loss/= k_lira
-            
             for data in self.train_loader:
-                defense_loss = self.train_temp_model(data,device,tmp_model,tempmodel_optimizer)
+                images,labels = data["image"],data["label"]
+                images,labels = images.to(device),labels.to(device)
+
+                # train target to backdoor the tmpmodel ( who is trained to be injected with the backdoor )
+                lira_optimizer.zero_grad()
+                poisoned_images = self.get_poisoned_batch(self.attack_model,images)
+                standard_attacked_labels = self.client_model(poisoned_images)
+                poisoned_labels = torch.tensor([self.label_replacement]*len(labels)).to(device)
+                attack_loss = self.criterion(standard_attacked_labels,poisoned_labels)
+                attack_loss.backward()
+                lira_optimizer.step()
                 with torch.no_grad():
-                    total_tempmodel_loss += defense_loss
+                    total_attack_loss += attack_loss
             print(f"Lira Epoch {epoch} : Attack Loss : {total_attack_loss} Defense Loss : {total_tempmodel_loss}")
+            
+    
+    # def train(self):
+        
+    #     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #     self.client_model.eval()
+    #     self.client_model.to(device)
+        
+    #     # prepare the hypothical model for the attacker to learn better
+    #     tmp_model = copy.deepcopy(self.client_model)
+    #     tmp_model.train()
+    #     tmp_model.to(device)
+    #     tempmodel_optimizer = torch.optim.SGD(tmp_model.parameters(),lr=self.client_model_optimizer_lr)
+        
+    #     lira_optimizer = torch.optim.SGD(self.attack_model.parameters(),lr=self.attacker_train_optimizer_lr)
+        
+    #     self.attack_model.to(device)
+    #     self.attack_model.train()
+    #     k_lira = 2
+    #     for epoch in range(self.attacker_train_epoch):
+    #         total_attack_loss = 0
+    #         total_tempmodel_loss = 0
+
+    #         for i in range(k_lira):
+    #             for data in self.train_loader:
+    #                 attack_loss = self.train_lira_batch(data,device,tmp_model,lira_optimizer)
+    #                 with torch.no_grad():
+    #                     total_attack_loss += attack_loss
+    #         total_attack_loss/= k_lira
+            
+    #         for data in self.train_loader:
+    #             defense_loss = self.train_temp_model(data,device,tmp_model,tempmodel_optimizer)
+    #             with torch.no_grad():
+    #                 total_tempmodel_loss += defense_loss
+    #         print(f"Lira Epoch {epoch} : Attack Loss : {total_attack_loss} Defense Loss : {total_tempmodel_loss}")
             
     def train_temp_model(self,data,device,tmp_model,tmp_optimizer):
         images,labels = data["image"],data["label"]
