60e0005108c378679a9326a35cf4f71bcbe18f1b
AdnenKhiari <adnenkhiari484@gmail.com>
2024-09-15 00:38:01 +0100

    cp

--- exps/safe/safe_clients_mobile_net.sh
+++ exps/safe/safe_clients_mobile_net.sh
@@ -3,41 +3,13 @@ script_path=$1
 
 # Run the Python command with the script path
 HYDRA_FULL_ERROR=1 && python "$script_path" \
-  num_gpus_per_client=0.5 \
-  num_cpus_per_client=2 \
-  num_rounds=40 \
-  num_clients=50 \
-  num_clients_per_round_fit=10 \
-  num_clients_per_round_eval=0 \
-  config_fit.local_epochs=3 \
-  config_fit.lr=0.008 \
-  optimizers=sgd \
-  dataset=breast \
-  model=resnet \
-  batch_size=512 \
-  poisoned_clients_ratio=0 \
-  poisoned_batch_size=64 \
-  global_merger=single \
-  poisoned_client=simple_patch \
-  grad_filter.active=false \
-  pgd_conf.active=false \
-  global_seed=42 \
-  wandb.active=true \
-  wandb.main_run_name="resnet_breast_iid" \
-  wandb.group_name="benign" \
-  wandb.project_name="federated_official"
-
-
-
-# Run the Python command with the script path
-HYDRA_FULL_ERROR=1 && python "$script_path" \
   num_gpus_per_client=0.25 \
   num_cpus_per_client=1 \
-  num_rounds=50 \
+  num_rounds=60 \
   num_clients=50 \
   num_clients_per_round_fit=10 \
   num_clients_per_round_eval=0 \
-  config_fit.local_epochs=3 \
+  config_fit.local_epochs=1 \
   config_fit.lr=0.008 \
   optimizers=sgd \
   dataset=breast \
@@ -53,31 +25,32 @@ HYDRA_FULL_ERROR=1 && python "$script_path" \
   wandb.active=true \
   wandb.main_run_name="mobilenet_breast_iid" \
   wandb.group_name="benign" \
-  wandb.project_name="federated_official"
+  wandb.project_name="federated_official_report"
 
 
-  # Run the Python command with the script path
 HYDRA_FULL_ERROR=1 && python "$script_path" \
-  num_gpus_per_client=0.5 \
-  num_cpus_per_client=2 \
-  num_rounds=40 \
+  num_gpus_per_client=0.25 \
+  num_cpus_per_client=1 \
+  num_rounds=80 \
   num_clients=50 \
   num_clients_per_round_fit=10 \
   num_clients_per_round_eval=0 \
-  config_fit.local_epochs=3 \
+  config_fit.local_epochs=1 \
   config_fit.lr=0.007 \
   optimizers=sgd \
   dataset=breast \
-  model=vit \
+  model=mobilenet \
   batch_size=256 \
   poisoned_clients_ratio=0 \
-  poisoned_batch_size=32 \
+  poisoned_batch_size=64 \
   global_merger=single \
   poisoned_client=simple_patch \
   grad_filter.active=false \
   pgd_conf.active=false \
+  partitioners=noniid \
+  partitioners.alpha=0.9 \
   global_seed=42 \
   wandb.active=true \
-  wandb.main_run_name="vit_breast_iid" \
+  wandb.main_run_name="mobilenet_breast_non_iid" \
   wandb.group_name="benign" \
-  wandb.project_name="federated_official"
+  wandb.project_name="federated_official_report"
