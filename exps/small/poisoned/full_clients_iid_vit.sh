0f6d5e56b4a6d92b41adf4eade916c8e5d6d9c20
AdnenKhiari <aymenkhiari484@gmail.com>
2024-09-15 16:25:36 +0100

    trying small experiments

---
+++ exps/small/poisoned/full_clients_iid_vit.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+script_path=$1
+
+# Run the Python command with the script path
+HYDRA_FULL_ERROR=1 && python "$script_path" \
+  num_gpus_per_client=0.5 \
+  num_cpus_per_client=2 \
+  num_rounds=100 \
+  num_clients=20 \
+  num_clients_per_round_fit=10 \
+  num_clients_per_round_eval=0 \
+  config_fit.local_epochs=2 \
+  config_fit.lr=0.007 \
+  optimizers=sgd \
+  dataset=breast \
+  model=vit \
+  batch_size=256 \
+  poisoned_clients_ratio=0.15 \
+  poisoned_batch_size=32 \
+  global_merger=single \
+  poisoned_client=frequency \
+  poisoned_client.norm_scaling_factor=1.2 \
+  grad_filter.active=true \
+  grad_filter.ratio=0.95 \
+  pgd_conf.active=true \
+  global_seed=42 \
+  wandb.active=true \
+  poison_between=[[18,50]] \
+  wandb.main_run_name="vit_breast_iid_full_attack" \
+  wandb.group_name="benign" \
+  wandb.project_name="official"
